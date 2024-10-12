acf98f2be74fe051e4016f545359f3700c53cd9d
Jakub Wójcik <superpralka@gmail.com>
2024-10-12 13:16:09 +0200

    Male zmiany

--- SzachyProjektSzachy/SzachowaGra/src/Board.java
+++ SzachyProjektSzachy/SzachowaGra/src/Board.java
@@ -6,26 +6,26 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Board extends Application {
-    private static final int TILE_SIZE = 100; // Rozmiar jednego pola
+    private static final int ROZMIAR_POLA = 100; // Rozmiar jednego pola
 
 
     public void start(Stage primaryStage) {
-        GridPane gridPane = new GridPane(); // Utworzenie siatki
+        GridPane szachownica = new GridPane(); // Utworzenie siatki
 
         // Ustawienie pól szachownicy
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
-                Rectangle tile = new Rectangle(TILE_SIZE, TILE_SIZE); // Utworzenie prostokąta jako pola
+                Rectangle tile = new Rectangle(ROZMIAR_POLA, ROZMIAR_POLA); // Utworzenie prostokąta jako pola
                 if ((i + j) % 2 == 0) {
                     tile.setFill(Color.BEIGE); // Kolor jasny
                 } else {
                     tile.setFill(Color.BROWN); // Kolor ciemny
                 }
-                gridPane.add(tile, j, i); // Dodanie pola do siatki
+                szachownica.add(tile, j, i); // Dodanie pola do siatki
             }
         }
 
-        Scene scene = new Scene(gridPane, TILE_SIZE * 8, TILE_SIZE * 8); // Utworzenie sceny
+        Scene scene = new Scene(szachownica, ROZMIAR_POLA * 8, ROZMIAR_POLA * 8); // Utworzenie sceny
         primaryStage.setTitle("Szaszki"); // Tytuł okna
         primaryStage.setScene(scene); // Ustawienie sceny
         primaryStage.show(); // Wyświetlenie okna
