724057b11a3429b02890ce0b46f263fc7068ee88
Jakub Wójcik <superpralka@gmail.com>
2024-10-12 13:04:58 +0200

    Dodanie funkcji która pokazuje biało-czerwoną szachownice

--- SzachyProjektSzachy/SzachowaGra/src/Main.java
+++ SzachyProjektSzachy/SzachowaGra/src/Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        liczDo10(); // Wywołanie funkcji liczDo10
-    }
-
-    // Funkcja licząca do 10
-    public static void liczDo10() {
-        for (int i = 1; i <= 15; i++) {
-            System.out.println(i); // Wypisuje aktualną wartość i
-        }
+        Board.main(args); // Wywołanie metody main w klasie Board
     }
 }
